1269a82fdf0397404f09b578d58485ce94863025
fella-sever <99533680+fella-sever@users.noreply.github.com>
2022-08-09 21:44:23 +0500

    cleanup olr arch from telegram cli

--- clients/telegram/telegram.go
+++ clients/telegram/telegram.go
@@ -1,44 +1,4 @@
 package telegram
 
-import (
-	"net/http"
-	"net/url"
-	"strconv"
-)
-
-type Client struct {
-	host     string // хост апи сервиса телеграма
-	basePath string // базовый путь - префикс, с которого идут все запросы
-	client   http.Client
-}
-
-//tg-bot.com/bot<token> -- хост с базовым путем
-
-func New(host, token string) Client {
-	return Client{
-		host:     host,
-		basePath: newBasePath(token),
-		client:   http.Client{},
-	}
-}
-
-/*мы делаем отдельную функцию для формирования пути для того, чтобы не бегать
-по коду и не менять все это дело руками в случае, если телега решит поменять
-вид основного пути*/
-
-func newBasePath(token string) (path string) {
-
-	return "bot" + token
-
-}
-
-func (c *Client) Updates(offset int, limit int) ([]Update, error) {
-	q := url.Values{}
-	q.Add("offset", strconv.Itoa(offset))
-	q.Add("limit", strconv.Itoa(limit))
-
-}
-
-func (c *Client) SendMessage() {
-
-}
+//https://api.telegram.org/bot<token>/METHOD_NAME.
+const requestURL string = "https://api.telegram.org/bot"
